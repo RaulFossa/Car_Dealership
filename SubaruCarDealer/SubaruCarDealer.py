bb3ce3fa599d831b4c1dc5f9194a8885fb52a56b
Raul Fossa <raul_andres@icloud.com>
2016-01-12 18:35:53 -0800

    "Added First Car Model costs"

--- SubaruCarDealer/SubaruCarDealer.py
+++ SubaruCarDealer/SubaruCarDealer.py
@@ -3,37 +3,73 @@
 ##01.12.2016
 
 def impreza_model():
-    print("SUBARU Impreza 2016")
-    print("Price: starting at $18,295")
-    print("Symmetrical All-Wheel Drive")
-    print("37-MPG Fuel Efficiency")
-    print("5 Door Body Style")
-    print("Leather-Trimmed Upholstery")
+    print("-SUBARU Impreza 2016 \n")
+    print("-Price: starting at $18,295")
+    print("-Symmetrical All-Wheel Drive")
+    print("-37-MPG Fuel Efficiency")
+    print("-5 Door Body Style")
+    print("-Leather-Trimmed Upholstery")
 
 def brz_model():
-    print("SUBARU BRZ 2016")
-    print("Price: starting at $25,395")
-    print("The ideal Sport Car")
-    print("Direct Injected Subaru Engine")
-    print("6-Speed Transmission")
-    print("Sport-Design Instrumentation")
+    print("-SUBARU BRZ 2016")
+    print("-Price: starting at $25,395")
+    print("-The ideal Sport Car")
+    print("-Direct Injected Subaru Engine")
+    print("-6-Speed Transmission")
+    print("-Sport-Design Instrumentation")
 
 def crosstrek_model():
-    print("SUBARU Crosstrek 2016")
-    print("Price: starting at $30,395")
-    print("Flexible Integrated Roof Rails")
-    print("42-MPG Fuel Efficiency")
-    print("Power Moonroof")
-    print("In-Vehicle Technology")
+    print("-SUBARU Crosstrek 2016")
+    print("-Price: starting at $30,395")
+    print("-Flexible Integrated Roof Rails")
+    print("-42-MPG Fuel Efficiency")
+    print("-Power Moonroof")
+    print("-In-Vehicle Technology")
 
 print("Hello and Welcome to SUBARU")
 init_ans = input("Are you ready to choose your new Car? ")
 
+## a fun game with user in case the answer is "NO"
 x = 0
 while init_ans.upper() == "NO":
-    print("really? I am going to ask you again!") # a fun game with user in case the answer is "NO"
+    print("really? I am going to ask you again!") 
     x += 1
     init_ans = input("Are you ready to choose your new Car? ")
+
+print(" ")
+print("Awesome! SUBARU CarDealer offers 3 Brand New 2016 Models.")
+print("Impreza, BRZ and Crosstrek \n")
+
+car_model = input("Which car SUBARU model would you like to buy? ")
+
+if car_model.upper() == "IMPREZA":
+    print(" ")
+    impreza_model()
+    print(" ")
+    dealer_fee = float(input("Please enter the Dealer Fee for this model: $"))
+    ask_tax = float(input("Please enter the tax percentage: "))
+    subtotal = 18295 + dealer_fee
+    tax = subtotal * ask_tax
+    total_car = subtotal + tax
+    print("Your Brand New SUBARU Impreza 2016 has a total of $", "%.2f" % total_car) 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
