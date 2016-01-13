3d186a9648cece21a11994e23ede6124faf8e06b
Raul Fossa <raul_andres@icloud.com>
2016-01-12 17:16:31 -0800

    "Created Functions"

--- SubaruCarDealer/SubaruCarDealer.py
+++ SubaruCarDealer/SubaruCarDealer.py
@@ -1,4 +1,34 @@
 ##Subaru CarDealer
 ##by raul_andres
-##12.31.2015
+##01.12.2016
+
+def impreza_model():
+    print("SUBARU Impreza 2016")
+    print("Price: starting at $18,295")
+    print("Symmetrical All-Wheel Drive")
+    print("37-MPG Fuel Efficiency")
+    print("5 Door Body Style")
+    print("Leather-Trimmed Upholstery")
+
+def brz_model():
+    print("SUBARU BRZ 2016")
+    print("Price: starting at $25,395")
+    print("The ideal Sport Car")
+    print("Direct Injected Subaru Engine")
+    print("6-Speed Transmission")
+    print("Sport-Design Instrumentation")
+
+def crosstrek_model():
+    print("SUBARU BRZ 2016")
+    print("Price: starting at $30,395")
+    print("Flexible Integrated Roof Rails")
+    print("37-MPG Fuel Efficiency")
+    print("Power Moonroof")
+    print("In-Vehicle Technology")
+
+def main()
+
+main()
+
+
 
